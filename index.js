040210122abc1028e110e53cce4a88d0ad7ec93a
maxper4 <maxenceperion@gmail.com>
2023-04-21 21:40:28 +0200

    Adapt to botviewer (ping, start, stop)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ipc = new ipcModule.IPC();
 
 ipc.config.id = "botsmanager";
 ipc.config.retry = 1500;
-ipc.config.silent = false;
+ipc.config.silent = true;
 ipc.config.networkPort = 8000;
 
 const setup = () => {
@@ -16,23 +16,34 @@ const setup = () => {
    
     ipc.serveNet(8001, () => 
         {
+            ipc.server.on("ping", (message, socket) => {
+                ipc.server.emit(socket, "pong", "pong");
+            });
+        
+            ipc.server.on("test-running", (message, socket) => {
+                ipc.server.emit(socket, "test-running", "good");
+            });
+
             ipc.server.on('start', (message, socket) => {
-                try {
-                    message = JSON.parse(message);
-                } catch (error) {
-                    console.log("Error while parsing message: " + error);
-                    return;
-                }
+                // try {
+                //     message = JSON.parse(message);
+                // } catch (error) {
+                //     console.log("Error while parsing message: " + error);
+                //     ipc.server.emit(socket, "started", {success: false});
+                //     return;
+                // }
 
                 if(message.bot && message.args) {
                     const bot = config.STARTABLE.find(x => x.name == message.bot);
                     if(!bot) {
                         sendAlert("Bot: " + message.bot + " not found");
+                        ipc.server.emit(socket, "started", {success: false});
                         return;
                     }
 
                     if(config.started[bot.name] && bot.singleton && config.started[bot.name].length > 0) {
                         sendAlert("[Bots Manager] Bot: " + bot.name + " already started");
+                        ipc.server.emit(socket, "started", {success: false});
                         return;
                     }
 
@@ -45,7 +56,6 @@ const setup = () => {
                     botProcess.unref();
 
                     console.log("Bot: " + bot.name + " started with PID: " + botProcess.pid);
-                    sendAlert("[Bots Manager] Bot: " + bot.name + " started.");
 
                     if(config.started[bot.name])
                         config.started[bot.name].push(botProcess.pid);
@@ -53,24 +63,30 @@ const setup = () => {
                         config.started[bot.name] = [botProcess.pid];
 
                     saveConfig();
+                    ipc.server.emit(socket, "started", {success: true, newList: getListStartable()});
+
+                    sendAlert("[Bots Manager] Bot: " + bot.name + " started.");
                 }
                 else {
                     console.log("Invalid message: " + message);
+                    ipc.server.emit(socket, "started", {success: false});
                 }
             });
 
             ipc.server.on('stop', (message, socket) => {
-                try {
-                    message = JSON.parse(message);
-                } catch (error) {
-                    console.log("Error while parsing message: " + error);
-                    return;
-                }
+                // try {
+                //     message = JSON.parse(message);
+                // } catch (error) {
+                //     console.log("Error while parsing message: " + error);
+                //     ipc.server.emit(socket, "stopped", {success: false});
+                //     return;
+                // }
 
                 if(message.bot) {
                     const bot = config.STARTABLE.find(x => x.name == message.bot);
                     if(!bot || !config.started[bot.name]) {
                         sendAlert("[Bots Manager] Bot: " + message.bot + " not found");
+                        ipc.server.emit(socket, "stopped", {success: false});
                         return;
                     }
 
@@ -83,9 +99,12 @@ const setup = () => {
 
                     config.started[bot.name] = [];
                     saveConfig();
+
+                    ipc.server.emit(socket, "stopped", {success: true, newList: getListStartable()});
                 }
                 else {
                     console.log("Invalid message: " + message);
+                    ipc.server.emit(socket, "stopped", {success: false});
                 }
             });
 
@@ -93,6 +112,10 @@ const setup = () => {
                 config = reloadModule("./config.json");
                 console.log("Config reloaded");
                 sendAlert("[Bots Manager] Config reloaded");
+            });
+
+            ipc.server.on("list", (message, socket) => {
+                ipc.server.emit(socket, "list", getListStartable());
             });
         }
     );
@@ -104,6 +127,10 @@ const setup = () => {
             console.log('Connected to contactor');
         });
     });
+}
+
+const getListStartable = () => {
+    return config.STARTABLE.filter(x => !(config.started[x.name] && config.started[x.name].length > 0 && x.singleton)).map(x => x.name)
 }
 
 const testAliveBots = () => {
